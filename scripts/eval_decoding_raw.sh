f5748b5c595f14adbfcde6bda48c2612b07ea44f
Omnia Ghonem <103151307+omnia-ghonem@users.noreply.github.com>
2024-11-04 03:55:44 +0200

    Update eval_decoding_raw.sh

--- scripts/eval_decoding_raw.sh
+++ scripts/eval_decoding_raw.sh
@@ -1,4 +1,4 @@
-python3 eval_decoding_raw.py \
+python /kaggle/working/EEG-to-Text-Decoding/eval_decoding_raw.py \
     --checkpoint_path /kaggle/input/train-eeg-t-text/checkpoints/decoding_raw/best/task1_task2_taskNRv2_finetune_BrainTranslator_skipstep1_b20_2_2_5e-05_5e-05_unique_sent.pt \
     --config_path /kaggle/input/train-eeg-t-text/config/decoding_raw/task1_task2_taskNRv2_finetune_BrainTranslator_skipstep1_b20_2_2_5e-05_5e-05_unique_sent.json \
     -cuda cuda:0
