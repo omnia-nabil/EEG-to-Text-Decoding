5f226783d3c912685274b19fd0690b10cc392923
Omnia Ghonem <103151307+omnia-ghonem@users.noreply.github.com>
2024-11-04 02:10:20 +0200

    Update train_decoding_raw.sh

--- scripts/train_decoding_raw.sh
+++ scripts/train_decoding_raw.sh
@@ -3,8 +3,8 @@ python /kaggle/working/EEG-to-Text-Decoding/train_decoding_raw.py --model_name B
     --one_step \
     --pretrained \
     --load_step1_checkpoint \
-    --num_epoch_step1 25 \
-    --num_epoch_step2 25 \
+    --num_epoch_step1 2 \
+    --num_epoch_step2 2 \
     -lr1 0.00005 \
     -lr2 0.00005 \
     -b 20\
